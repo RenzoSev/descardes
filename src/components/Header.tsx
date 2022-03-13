6c5f6409ab18fff3cd02426557a44a0c09c7d389
Renzo Sevilha <sevilharenzo@gmail.com>
2022-03-13 12:32:30 -0300

    fix(header): changes bg color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
-function Header() {
+import React from 'react';
+
+const Header: React.FC = () => {
   return (
-    <section className="w-full rounded-sm bg-cyan-300">
+    <section className="w-full rounded-sm bg-blue-500">
       <header className="px-4 py-2">
         <h1 className="text-3xl font-bold text-slate-100">Decardes</h1>
       </header>
     </section>
   );
-}
+};
 
 export default Header;

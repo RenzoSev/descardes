3073458fcb87902e662240c2949005f5b2cf4c96
Renzo Sevilha <sevilharenzo@gmail.com>
2022-03-13 15:31:44 -0300

    feat: creates card types

---
+++ src/types/Cards.tsx
@@ -0,0 +1,23 @@
+export interface BaseCard {
+  title: string;
+  answer: string;
+  question: string;
+  content?: any;
+}
+
+export interface SideCard extends BaseCard {
+  type: 'side';
+}
+
+export interface EssayCard extends BaseCard {
+  type: 'essay';
+}
+
+export interface ChooseCard extends BaseCard {
+  type: 'choose';
+  content: {
+    options: { title: string; isAnswer: true }[];
+  };
+}
+
+export type Card = ChooseCard | SideCard | EssayCard;

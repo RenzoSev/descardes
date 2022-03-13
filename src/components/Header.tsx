7f2d2f52890c8d7202d51c96cedf4574cbc27adc
Renzo Sevilha <sevilharenzo@gmail.com>
2022-03-13 12:03:19 -0300

    feat: header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <section className="w-full rounded-sm bg-cyan-300">
+      <header className="px-4 py-2">
+        <h1 className="text-3xl font-bold text-slate-100">Decardes</h1>
+      </header>
+    </section>
+  );
+}
+
+export default Header;

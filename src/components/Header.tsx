d42f0f9360c51cb7d2ce248c02375ca30de2b7af
Renzo Sevilha <sevilharenzo@gmail.com>
2022-03-13 15:31:50 -0300

    fix(header): changes styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <section className="w-full rounded-sm bg-blue-500">
+    <section className="w-full rounded-b-sm bg-blue-500">
       <header className="px-4 py-2">
         <h1 className="text-3xl font-bold text-slate-100">Decardes</h1>
       </header>

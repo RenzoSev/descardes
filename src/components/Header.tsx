bd8d0addc6a877449ff16f6c8960fa05a3de6131
Renzo Sevilha <sevilharenzo@gmail.com>
2022-03-13 17:01:51 -0300

    chore: changes wrong name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header: React.FC = () => {
   return (
     <section className="w-full rounded-b-sm bg-blue-500">
       <header className="px-4 py-2">
-        <h1 className="text-3xl font-bold text-slate-100">Decardes</h1>
+        <h1 className="text-3xl font-bold text-slate-100">Descardes</h1>
       </header>
     </section>
   );

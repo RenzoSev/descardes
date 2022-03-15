4785902f5712cd12c1fba60617c8b03e98a5e43e
Renzo Sevilha <sevilharenzo@gmail.com>
2022-03-15 20:18:32 -0300

    feat(Header): adds subtitle props

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
-const Header: React.FC = () => {
+interface HeaderProps {
+  title: string;
+  subtitle?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ title, subtitle }) => {
   return (
     <section className="w-full rounded-b-sm bg-blue-500">
-      <header className="px-4 py-2">
-        <h1 className="text-3xl font-bold text-slate-100">Descardes</h1>
+      <header className="flex items-center justify-between px-4 py-2">
+        <h1 className="text-3xl font-bold text-slate-100">{title}</h1>
+        {subtitle && (
+          <h2 className="text-lg font-bold text-slate-100">{subtitle}</h2>
+        )}
       </header>
     </section>
   );

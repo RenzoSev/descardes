346f36196bc041276d83beaa3bd37cee5b8ed6ce
Renzo Sevilha <sevilharenzo@gmail.com>
2022-03-15 20:19:44 -0300

    feat(Home): adds title header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import Header from '../components/Header';
 const Home: React.FC = () => {
   return (
     <>
-      <Header />
+      <Header title="Descardes" />
       <GroupCards />
     </>
   );

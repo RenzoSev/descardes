a16ef0dab281e1ee831fa4463f0aa86e14975db3
Renzo Sevilha <sevilharenzo@gmail.com>
2022-03-16 20:42:10 -0300

    fix(types): adds id into Cards

--- src/types/Cards.ts
+++ src/types/Cards.ts
@@ -1,8 +1,12 @@
+export type CardTypes = 'side' | 'essay' | 'choose';
+
 export interface BaseCard {
+  id: number;
   title: string;
   answer: string;
   question: string;
   content?: any;
+  type: CardTypes;
 }
 
 export interface SideCard extends BaseCard {
@@ -13,14 +17,15 @@ export interface EssayCard extends BaseCard {
   type: 'essay';
 }
 
+export interface ContentChooseCard {
+  options: { title: string; isAnswer: true }[];
+}
 export interface ChooseCard extends BaseCard {
   type: 'choose';
-  content: {
-    options: { title: string; isAnswer: true }[];
-  };
+  content: ContentChooseCard;
 }
 
-export type Card = ChooseCard | SideCard | EssayCard;
+export type Card = SideCard | EssayCard | ChooseCard;
 
 export interface GroupCard {
   id: number;

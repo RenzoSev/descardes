2a0ede9a574bd0c0272f3db6b8e93551dd8b2fda
Renzo Sevilha <sevilharenzo@gmail.com>
2022-03-16 23:39:40 -0300

    feat(grid): adds styles support to details

--- src/components/Grid/Details.tsx
+++ src/components/Grid/Details.tsx
@@ -1,9 +1,13 @@
 interface GridDetails {
   details: string;
+  styles?: string;
 }
 
-const GridDetails: React.FC<GridDetails> = ({ details }) => {
-  return <p className="text-sm text-slate-700/50">{details}</p>;
+const GridDetails: React.FC<GridDetails> = ({ details, styles = '' }) => {
+  const BASE_CLASS_NAME = 'text-sm text-slate-700/50';
+  const className = BASE_CLASS_NAME + ' ' + styles;
+
+  return <p className={className}>{details}</p>;
 };
 
 export default GridDetails;

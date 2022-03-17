9c61ba3bca831d6ed0dcd5aa83e7d73caced9b4d
Renzo Sevilha <sevilharenzo@gmail.com>
2022-03-16 22:35:43 -0300

    feat(tailwind.config.js): adds new breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      'xs': '400px',
+      ...defaultTheme.screens,
+    },
     extend: {
       spacing: {
         86: '22rem',

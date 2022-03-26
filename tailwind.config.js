df46c46746b84dc86d95365e14b72fb32a01630c
Renzo Sevilha <sevilharenzo@gmail.com>
2022-03-25 22:56:53 -0300

    feat(tailwind.config): adds some font styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     screens: {
-      'xs': '400px',
+      xs: '400px',
       ...defaultTheme.screens,
     },
     extend: {
       spacing: {
         86: '22rem',
+      },
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
       },
     },
   },

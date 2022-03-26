55eac674bb4cc90d4ddceeba90e17b8e906cb2a8
Renzo Sevilha <sevilharenzo@gmail.com>
2022-03-25 22:57:53 -0300

    fix: changes height to cover full screen

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,7 +1,15 @@
-const MainContainer: React.FC = ({ children }) => {
-  return (
-    <section className="flex flex-col gap-8 px-4 py-8">{children}</section>
-  );
+interface MainContainerProps {
+  containerClassName?: string;
+}
+
+const MainContainer: React.FC<MainContainerProps> = ({
+  children,
+  containerClassName,
+}) => {
+  const SECTION_BASE_CLASS_NAME = 'flex h-full flex-col gap-8 px-4 py-8';
+  const SECTION_CLASS_NAME = SECTION_BASE_CLASS_NAME + ' ' + containerClassName;
+
+  return <section className={SECTION_CLASS_NAME}>{children}</section>;
 };
 
 export default MainContainer;

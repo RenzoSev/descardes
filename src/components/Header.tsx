d014a0d1ab5650cc90577a2e371e0776058e804d
Renzo Sevilha <sevilharenzo@gmail.com>
2022-03-27 09:04:24 -0300

    feat(header): adds previous page click

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface HeaderProps {
   title: string;
@@ -7,16 +8,21 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ title, subtitle, centerTitle }) => {
+  const navigate = useNavigate();
+
   const BASE_HEADER_CLASS_NAME = 'flex items-center px-4 py-2';
   const JUSTIFY_BY_POSITION = centerTitle
-    ? 'justify-center'
+    ? 'justify-center text-center'
     : 'justify-between';
   const HEADER_CLASS_NAME = BASE_HEADER_CLASS_NAME + ' ' + JUSTIFY_BY_POSITION;
 
   return (
     <section className="w-full rounded-b-sm bg-blue-500">
       <header className={HEADER_CLASS_NAME}>
-        <h1 className="text-3xl font-bold tracking-wide text-slate-100">
+        <h1
+          className="text-3xl font-bold tracking-wide text-slate-100"
+          onClick={() => navigate(-1)}
+        >
           {title}
         </h1>
         {subtitle && (
